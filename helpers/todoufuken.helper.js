ac86930b735f44a81f2255ea0ef740b4179c926b
yohei1226 <kundagpwm1226@gmail.com>
2022-10-29 15:14:21 +0900

    Nuxtプロジェクト作成　ESLint Prettierの設定　apiKeyの設定　都道府県一覧APIでデータ取得

---
+++ helpers/todoufuken.helper.js
@@ -0,0 +1,7 @@
+export const CITIES = [{
+  city: 'kagoshima'
+}, {
+  city: 'okinawa'
+}, {
+  city: 'fukuoka'
+}];
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>テスト</div>
+</template>
+
+<script>
+export default {
+  name: 'PopulationTransitionAppIndex',
+  data() {
+    return {
+      test: 'yohei',
+    }
+  },
+  computed: {},
+  async asyncData({ $axios, $config }) {
+    console.log($config.apiKey)
+    const url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures'
+    const response = await $axios.$get(url, {
+      headers: { 'X-API-KEY': $config.apiKey },
+    })
+    return {
+      posts: response,
+    }
+  },
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
